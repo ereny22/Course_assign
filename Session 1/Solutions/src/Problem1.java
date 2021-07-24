d5b626a9fcb6782ce6a8e4153e7b0052a09381d3
ereny22 <81209944+ereny22@users.noreply.github.com>
2021-07-24 12:44:49 +0300

    Update Problem1.java

--- Session 1/Solutions/src/Problem1.java
+++ Session 1/Solutions/src/Problem1.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 public class Problem1 {
     public static void main(String [] args){
         // Write your code here
+        int a  ;
+       int b  ;
+       int count = 0;
+      int i =0;
+       
+        Scanner input = new Scanner (System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+     
+       while (i < 10){
+      if (a <= b){
+          count++;
+         a = a*3 + a;
+         b = b*2 + b ;
+      }
+      else{
+          System.out.println (count);
+          break;
+      }
+      i++;
+        
+       }
     }
 }
